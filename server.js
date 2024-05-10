e46e8e771c557f6c9baef2df747f9dfd6cdeae19
Captain Patel <k2patel4947@gmail.com>
2024-05-10 22:13:50 +0530

    commit 1

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.API_KEY);
+
+
+
+app.get("/getNews/:tag?", async (req, res) => {
+    try {
+        const tag = req.params.tag || '';
+        console.log(tag);
+        const options = {
+            language: 'en',
+            country: 'in',
+        };
+        if (tag) {
+            options.category = tag; // Include category only if there is a tag 
+        }
+        newsapi.v2.topHeadlines(options).then(response => {
+            // 10 articles is all we need for the frontend
+            const data = response.articles.slice(0, 10);
+            console.log("sent data");
+            res.json({
+                data
+            });
+        });
+    } catch (error) {
+        console.log(error);
+        res.send("error");
+    }
+});
+
+
+app.listen(5000, () => {
+    console.log("Listening on port 5000");
+})
